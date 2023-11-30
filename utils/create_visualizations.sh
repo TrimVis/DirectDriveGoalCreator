8da00e47dd191296bfbfdf95b02a6f6ba4d5df3b
Pasquale Jordan <jordanpa@ethz.ch>
2023-11-30 15:43:03 +0100

    Fixed mistake

--- utils/create_visualizations.sh
+++ utils/create_visualizations.sh
@@ -4,11 +4,12 @@ BINS="$1"
 SRC_DIR="$2"
 
 mkdir -p "$SRC_DIR/viz"
+mkdir -p "$SRC_DIR/trace"
 
 for f in "$SRC_DIR"/*.goal; do
     BNAME=$(basename "$f" .goal)
     BPATH_VIZ="${SRC_DIR}/viz/${BNAME}.viz"
-    BPATH_TRACE="${SRC_DIR}/viz/${BNAME}.trace"
+    BPATH_TRACE="${SRC_DIR}/trace/${BNAME}.trace"
 
     echo "$BNAME"; 
     "$BINS"/txt2bin -i "$f" -o temp.bin \
